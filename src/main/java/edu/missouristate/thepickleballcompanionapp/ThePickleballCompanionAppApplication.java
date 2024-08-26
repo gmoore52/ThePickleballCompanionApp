415d043b4fa2b08dc5b20c4d5ff89f317c253ff4
gmoore52 <gavinmoore5225@gmail.com>
2024-08-26 16:49:13 -0500

    Init commit

---
+++ src/main/java/edu/missouristate/thepickleballcompanionapp/ThePickleballCompanionAppApplication.java
@@ -0,0 +1,13 @@
+package edu.missouristate.thepickleballcompanionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePickleballCompanionAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThePickleballCompanionAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/missouristate/thepickleballcompanionapp/ThePickleballCompanionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.missouristate.thepickleballcompanionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThePickleballCompanionAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
